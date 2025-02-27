66a26f32ed487447bb09a1454f1611e2f4d35e8f
Kalyani Mavale <156056228+kalyanimavale@users.noreply.github.com>
2025-02-27 14:37:47 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
+# Author: Kalyani Mavale
+# Additional Authors:
+# <kalyanimavale>
+
+# Input:
+# p, principal amount
+# t, time period in years
+# r, annual rate of interest
+
+# Output:
+# simple interest = p*t*r/100
+
+echo "Enter the principal:"
+read p
+echo "Enter rate of interest per year:"
+read r
+echo "Enter time period in years:"
+read t
+
+s=$((p * t * r / 100))
+
+echo "The simple interest is: $s"
 
